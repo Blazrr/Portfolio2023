d027c2f3721cfd757c6ee4fd2493da22a5e8f4d0
blazrr <selmhamedi@gmail.com>
2023-03-08 22:08:01 +0100

    Before Projects

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ import { Provider, useDispatch } from "react-redux";
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isShown, setIsShown] = useState<boolean>(false);
+  const [isShown, setIsShown] = useState<boolean>(true);
   const router = useRouter();
   useEffect(() => {
     setTimeout(() => setIsShown(false), 2800);
