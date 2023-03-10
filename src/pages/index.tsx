aabe0bd8a6634143873019f9e1ebdec8fbbcbe5b
blazrr <selmhamedi@gmail.com>
2023-03-10 14:08:26 +0100

    Fixed the scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,37 +9,41 @@ import Cursor from "@/components/Commons/Cursor";
 import Footer from "@/components/Index/Footer";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
-    const goToProject = (e:any) => {
-      if (e?.target?.body?.clientWidth > 500){
-        router.push("Projects")
+    const goToProject = (e: any) => {
+      if (e?.target?.body?.clientWidth > 500) {
+        router.push("Projects");
       }
-      console.log(e?.target?.body?.clientWidth)
+      console.log(e?.target?.body?.clientWidth);
     };
     document.addEventListener("scroll", goToProject, true);
 
     return () => document.removeEventListener("scroll", goToProject, true);
   }, [router]);
   return (
-    <motion.div className=" relative w-4/5 mx-auto"
-    initial={{x:"-100%", opacity: 0}}
-    animate={{x:0 ,opacity: 1}}
-    exit={{x:"100%",opacity: 0}}
-    transition={{ ease: "easeOut",duration: .8 }} >
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Sami's Portfolio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Scroll to Discover</title>
-        <link rel="icon" href="/logo.png" />
-      </Head>
-      <div className="absolute h-full w-full md:-bottom-16 -bottom-2 lg:-bottom-32 xl:-bottom-48  left-0">
-      <Header />
-      <Footer/>
-      <div className="mt-64 hidden lg:block">You found it !</div>
-      </div>
+    <>
+      <motion.div
+        className=" relative w-4/5 mx-auto"
+        initial={{ x: "-100%", opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: "100%", opacity: 0 }}
+        transition={{ ease: "easeOut", duration: 0.8 }}
+      >
+        <Head>
+          <title>Create Next App</title>
+          <meta name="description" content="Sami's Portfolio" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Scroll to Discover</title>
+          <link rel="icon" href="/logo.png" />
+        </Head>
+      </motion.div>
+        <div className="fixed h-full w-4/5 -translate-x-1/2 left-1/2  md:-bottom-16 -bottom-2 lg:-bottom-32 xl:-bottom-48  transform">
+          <Header />
+          <Footer />
+        </div>
 
-    </motion.div>
+        <span className="absolute -bottom-12 left-0 text-transparent">Uou found it</span>
+    </>
   );
 }
