d1891e1d466b88b2567c3a3c186d1dc9cca1e015
blazrr <selmhamedi@gmail.com>
2023-03-12 17:54:43 +0100

    Fixed the Background

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ import Darkmode from "@/components/Commons/Darkmode";
 // Create your instance
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isShown, setIsShown] = useState<boolean>(true);
+  const [isShown, setIsShown] = useState<boolean>(false);
   const router = useRouter();
   useEffect(() => {
     setTimeout(() => setIsShown(false), 2800);
