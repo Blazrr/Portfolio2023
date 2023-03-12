b64f48dafcad59bf3ff97fd62210476fb5621bce
blazrr <selmhamedi@gmail.com>
2023-03-12 21:14:30 +0100

    test

--- src/components/Commons/CustomBackground.tsx
+++ src/components/Commons/CustomBackground.tsx
@@ -8,9 +8,9 @@ const CustomBackground = (props: Props) => {
   const { resolvedTheme } = useTheme()
     const gradient: any = new Gradient();
     gradient.initGradient("#gradient-canvas");
-    const darkStyle = {   "--gradient-color-1": "#043D5D !important","--gradient-color-2": "#032E46 !important","--gradient-color-3": "#23B684 !important ","--gradient-color-4 !important": "#0F595E !important",} as React.CSSProperties;
+    const darkStyle = {   "--gradient-color-1": "#043D5D ","--gradient-color-2": "#032E46 ","--gradient-color-3": "#23B684 ","--gradient-color-4": "#0F595E ",} as React.CSSProperties;
 
-    const lightStyle = {   "--gradient-color-1": "#c3e4ff !important ","--gradient-color-2": "#6ec3f4 !important","--gradient-color-3": "#eae2ff !important","--gradient-color-4": "#b9beff !important",} as React.CSSProperties;
+    const lightStyle = {   "--gradient-color-1": "#c3e4ff ","--gradient-color-2": "#6ec3f4 ","--gradient-color-3": "#eae2ff ","--gradient-color-4": "#b9beff ",} as React.CSSProperties;
 
 
     console.log(resolvedTheme)
