5848f2f740a6e75cd0ff046a2da0db818dbf0307
blazrr <selmhamedi@gmail.com>
2023-03-12 20:53:36 +0100

    using resolvedTheme for checking theme

--- src/components/Commons/Darkmode.tsx
+++ src/components/Commons/Darkmode.tsx
@@ -7,14 +7,18 @@ type Props = {}
 
 const Darkmode = (props: Props) => {
     const {theme, setTheme} = useTheme()
+    const { resolvedTheme } = useTheme()
 
     const changeTheme = () => {
-        if (theme === "light") setTheme("dark")
+        if (resolvedTheme === "light") setTheme("dark")
         else setTheme("light")       
     }
   return (
-    <button className='fixed top-6 right-6 rounded p-2 border-2 border-black dark:border-white' onClick={changeTheme}>{theme === "light" ? <FaMoon/> : <FaSun/>}
+    <>
+    <h1>{theme}</h1>
+    <button className='fixed top-6 right-6 rounded p-2 border-2 border-black dark:border-white' onClick={changeTheme}>{resolvedTheme === "light" ? <FaMoon/> : <FaSun/>}
     </button>
+    </>
   )
 }
 
