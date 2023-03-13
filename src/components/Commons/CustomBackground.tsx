5d7271338277c99ed1c94056504790afb23e45fc
blazrr <selmhamedi@gmail.com>
2023-03-13 01:34:19 +0100

    ClassNames for darkmode

--- src/components/Commons/CustomBackground.tsx
+++ src/components/Commons/CustomBackground.tsx
@@ -8,11 +8,10 @@ const CustomBackground = (props: Props) => {
   const { resolvedTheme } = useTheme()
     const gradient: any = new Gradient();
     gradient.initGradient("#gradient-canvas");
-    const darkStyle = {   "--gradient-color-1": "#043D5D ","--gradient-color-2": "#032E46 ","--gradient-color-3": "#23B684 ","--gradient-color-4": "#0F595E ",} as React.CSSProperties;
-    const lightStyle = {   "--gradient-color-1": "#c3e4ff ","--gradient-color-2": "#6ec3f4 ","--gradient-color-3": "#eae2ff ","--gradient-color-4": "#b9beff ",} as React.CSSProperties;
+
 
   return (
-    <canvas id="gradient-canvas" style={resolvedTheme != "light" ? darkStyle : lightStyle} data-transition-in data-js-darken-top /> 
+    <canvas id="gradient-canvas" className='gradient-dark dark:gradient-light' data-transition-in data-js-darken-top /> 
   )
 }
 
