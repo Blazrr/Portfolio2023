734821aa7861cfc60da9204b86f005544bdac0d5
blazrr <selmhamedi@gmail.com>
2023-03-23 14:57:14 +0100

    DarkMode color and font

--- src/components/Commons/Darkmode.tsx
+++ src/components/Commons/Darkmode.tsx
@@ -1,6 +1,8 @@
+import { changeCursor } from '@/reducers/cursorSlice'
 import { useTheme } from 'next-themes'
 import React from 'react'
 import {FaSun, FaMoon} from "react-icons/fa"
+import { useDispatch } from 'react-redux'
 
 
 type Props = {}
@@ -10,13 +12,18 @@ const Darkmode = (props: Props) => {
     const { resolvedTheme } = useTheme()
 
     const changeTheme = () => {
+        textLeave()
         if (resolvedTheme === "light") setTheme("dark")
         else setTheme("light")       
     }
+
+    const dispatch = useDispatch()
+    const textEnter = () => dispatch(changeCursor("text"))
+    const textLeave = () => dispatch(changeCursor("default"))
   return (
 
 
-    <button className='fixed top-6 right-6 rounded p-2 border-2 border-black dark:border-white z-[9001]' onClick={changeTheme}>{resolvedTheme === "light" ? <FaMoon/> : <FaSun/>}
+    <button className='fixed top-6 right-6 rounded p-2 border-2 border-black dark:border-white z-[9001]' onClick={changeTheme} onMouseOver={textEnter} onMouseLeave={textLeave} >{resolvedTheme === "light" ? <FaMoon/> : <FaSun/>}
     </button>
 
   )
